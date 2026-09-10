133bec783627035de2e2e705534ac0e4aa7993ab
swpcth <swpc.thseminar@gmail.com>
2026-09-10 08:59:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ---------------------------------------------------------------
+// ตั้งค่าเพียงจุดเดียว: วาง URL ของ Apps Script Web App ที่นี่
+// ได้ URL นี้หลังจากทำตามขั้นตอนใน README.md ข้อ "Deploy Apps Script"
+// ตัวอย่าง: https://script.google.com/macros/s/AKfycb.../exec
+// ---------------------------------------------------------------
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzdwEin_udnC88u6ISaP_jDt2Han-uZq1EiFNKKzUPuKQ9Irw4f4dqGO9vu_wiC2Krx/exec";
